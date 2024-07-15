45026dded2888dd2a2fd854faeff649a17575cef
Jovan Boagni <jovan@Jovans-MacBook-Air.local>
2024-07-14 22:53:32 -0700

    Simplify component

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -1,112 +1,87 @@
-import React, { useRef, useState, useEffect , useContext} from 'react';
+import React, { useRef, useState, useEffect, useContext } from 'react';
 import Playback from '../components/Playback';
 import StateManager from '../components/StateManager';
 import GameOfLife from '../logic/GameOfLife';
-
 import { AppContext } from '../AppContext';
 
 function GameOfLifeComp() {
-
-    const {
-        state, 
-        gridLines, 
-        dimensions, 
-        setDimensions,
-        height, 
-        width, 
-    } = useContext(AppContext);
-
-    const canvasRef = useRef( null );
+    const { gridLines, dimensions, height, width } = useContext(AppContext);
+    const canvasRef = useRef(null);
     const gameRef = useRef(null);
-    const [speed, setSpeed ] = useState( 10 );
-    const [playing, setPlaying ] = useState(null);
+    const [speed, setSpeed] = useState(10);
+    const [playing, setPlaying] = useState(null);
 
-    function setNewState( { initialState }) {
+    const setNewState = (initialState) => {
+
+        initialState ??=  GameOfLife.random(dimensions[0]);
+
         const canvas = canvasRef.current;
         if (canvas) {
-            if ( gameRef.current ) {
+            if (gameRef.current) {
                 gameRef.current.cleanup();
             }
-            gameRef.current = new GameOfLife({ canvas, initialState, speed ,gridLines});
+            gameRef.current = new GameOfLife({ canvas, initialState, speed, gridLines });
             gameRef.current.render();
         }
-    }
+    };
 
     useEffect(() => {
-        const initialState = GameOfLife.random( dimensions[0]);
-        setNewState( { initialState } );
+        setNewState();
+    }, [gameRef, canvasRef, dimensions ]);
 
-    }, [gameRef, canvasRef, dimensions, state]);
+    useEffect(() => {
+        if (gameRef.current) {
+            gameRef.current.playing = playing;
+        }
+    }, [playing]);
 
-    useEffect( () => {
-        gameRef.current.playing = playing;
-    }, [playing] );
+    useEffect(() => {
+        if (gameRef.current) {
+            gameRef.current.speed = speed;
+        }
+    }, [speed]);
 
-    useEffect( () => {
-        gameRef.current.speed = speed;
-    }, [speed] )
+    useEffect(() => {
+        if (gameRef.current) {
+            gameRef.current.gridLines = gridLines;
+        }
+    }, [gridLines]);
 
-
-    useEffect( () => {
-        gameRef.current.gridLines = gridLines;
-
-    });
-
-    function forward() {
+    const forward = () => {
         setPlaying(false);
         gameRef.current.nextState();
-    }
+    };
 
-    function back() {
+    const back = () => {
         setPlaying(false);
         gameRef.current.previousState();
-    }
+    };
 
-    function reset() {
+    const reset = () => {
         setPlaying(false);
         gameRef.current.reset();
-    }
+    };
 
-    function toggleStart() {
+    const toggleStart = () => {
         setPlaying(!playing);
-
-    }
-
-    function getRandomState() {
-        const initialState = GameOfLife.random( dimensions[0] );
-        setNewState( { initialState } )
-    }
-
-    function getStateWithNewDimensions([w,h]) {
-        setDimensions([w,h]);
-    }
+    };
 
     return (
         <div id="gameoflife">
-            <canvas 
-                ref={canvasRef} 
-                height={`${height}`} 
-                width={`${width}`}
-                onClick={toggleStart}
-            />
-
-            <Playback 
+            <canvas ref={canvasRef} height={`${height}`} width={`${width}`} onClick={toggleStart} />
+            <Playback
                 toggleStart={toggleStart}
-                forward={forward} 
-                back={back} 
-                reset={reset} 
-                speed={speed} 
-                setSpeed={setSpeed} 
+                forward={forward}
+                back={back}
+                reset={reset}
+                speed={speed}
+                setSpeed={setSpeed}
                 playing={playing}
                 setPlaying={setPlaying}
             />
-
-            <StateManager 
-                getRandomState={ getRandomState }
-            />
-
+            <StateManager getRandomState={ () => setNewState() } />
         </div>
-    )
+    );
 }
 
 export default GameOfLifeComp;
